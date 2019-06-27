361d7142c58a1b0d33fcba87b0815bdbeeac7339
christian sandström <christian.sandstrom@appliedtechnology.se>
2019-06-27 17:53:30 +0200

    everything working e2e, but doesnt check user input

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const router = express.Router();
 const getUrl = require('../urlHandler/getUrl/index')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Handlebars with Express 4' });
 });
-router.get('/:id', function(req, res, next) {
-  console.log('req.params: ', req.params.id);
-  
-let redirectUrl = getUrl.getUrl()
-console.log(redirectUrl);
-  
-  res.redirect(redirectUrl);
+router.get('/:id', async function (req, res, next) {
+  let link = await getUrl.getUrl(req.params.id)
+  res.redirect(link);
 });
 
 module.exports = router;
--- urlHandler/getUrl/index.js
+++ urlHandler/getUrl/index.js
@@ -1,5 +1,17 @@
-function getUrlService(id) {
-  const url = 'http://www.google.se'
-  return url
+const fs = require('fs');
+
+async function getUrlService(id) {
+  let url = await readDb(id)  
+  return JSON.parse(url).find(rows => rows.id === id).url
 }
+
+function readDb(id) {
+  return new Promise((resolve, reject) => {
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) return reject(err);
+    resolve(data);
+  });
+  })
+}
+
 module.exports.getUrl = getUrlService
