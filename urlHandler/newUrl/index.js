494e00e97d282dd2e1e718d8c7968c15c4e5319e
christian sandström <christian.sandstrom@appliedtechnology.se>
2019-06-27 10:00:06 +0200

    dev file and json db structure setup

---
+++ urlHandler/newUrl/index.js
@@ -0,0 +1,17 @@
+function urlService(orgUrl) {
+  const result = {url : 'bajs'}
+  return result;
+}
+function validator () {
+
+}
+
+function generateId() {
+
+}
+
+function writeDb () {
+
+}
+
+module.exports.urlService = urlService;
