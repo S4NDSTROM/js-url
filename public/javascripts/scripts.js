b39938afeff208dcdaa6ab28002971bd228fdbdb
christian sandström <christian.sandstrom@appliedtechnology.se>
2019-06-27 20:21:18 +0200

    added CSS and other visuals - good night

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,17 +1,16 @@
 console.log("Hello")
 
-//fetch, preventdefault etc
 window.onload = function () {
-  let data = {username: 'example'};
+  let data = { username: 'example' };
   const submitBtn = document.querySelector('#submit-btn').addEventListener("click", (event) => {
     event.preventDefault()
     let userInput = document.querySelector('#user-input').value
     fetch('http://localhost:3000/api/newurl', {
       method: 'POST',
-            headers: {
-            'Content-Type': 'application/json',
-        },
-      body: JSON.stringify({userInput : userInput}),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput: userInput }),
     })
       .then(
         function (response) {
@@ -22,6 +21,13 @@ window.onload = function () {
           }
           // Examine the text in the response
           response.json().then((data) => {
+            if (data === 'Invalid Url') {
+              document.querySelector('#user-input').value = data;
+              document.querySelector('#user-input').style.cssText = "background-color: red; border: 2px solid black; border-radius: 4px;";
+            } else {
+              document.querySelector('#user-input').value = data;
+              document.querySelector('#user-input').style.cssText = "background-color: green; border: 2px solid black; border-radius: 4px;";
+            }
             document.querySelector('#user-input').value = data;
           });
         }
